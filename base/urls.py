276b6e7af5a5b59e62b20c27241e7ac1ea09a4a4
Santiago <48573143+RovleZMex@users.noreply.github.com>
2023-09-14 08:56:23 -0700

    Added message CRUD operations and diplay of room participants.

--- base/urls.py
+++ base/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('create-room/', views.create_room, name="create-room"),
     path('update-room/<str:pk>', views.update_room, name="update-room"),
     path('delete-room/<str:pk>', views.delete_room, name='delete-room'),
+    path('delete-message/<str:pk>', views.delete_message, name="delete-message"),
 
     path('login/', views.login_page, name="login"),
     path('logout/', views.logout_user, name="logout"),
--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db.models import Q
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.contrib.auth.models import User
 from django.contrib import messages
@@ -76,7 +76,18 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by("-created")
+    participants = room.participants.all()
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 
@@ -120,4 +131,17 @@ def delete_room(request, pk):
         room.delete()
         return redirect('home')
     context = {'obj': room}
+    return render(request, 'base/delete.html', context)
+
+@login_required(login_url='login')
+def delete_message(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!!')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    context = {'obj': message}
     return render(request, 'base/delete.html', context)
